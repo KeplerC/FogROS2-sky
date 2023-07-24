ed6705e713830e36f76cb03cb3cb294cfb79ae88
kushtimusPrime <kush_hari@berkeley.edu>
2023-07-24 15:40:36 -0700

    Got plan figured out

---
+++ fogros2/scripts/test_optimization.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello World!")
+
+    # Start with reading the benchmark.yaml file
+    # Get the max, min, and middle CPU values
+    # Run benchmarking these 3 values
+    # Find linear function for seconds per step
+    # Find these exact instances in the ~/.sky/catalogs/v5/aws/vms.csv
+    # Figure out cost per CPU by looking at those instances (we could just pull from terminal, but that may not scale well)
+    # Setup optimization
+
+if __name__ == "__main__":
+    main()
