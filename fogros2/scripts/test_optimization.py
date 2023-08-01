4d540a42e26e615d8c9e94f670412e7a264878a3
kushtimusPrime <kush_hari@berkeley.edu>
2023-08-01 14:58:29 -0700

    Generates the YAML files necessary for benchmarking

--- fogros2/scripts/test_optimization.py
+++ fogros2/scripts/test_optimization.py
@@ -1,45 +1,91 @@
 import argparse
 import os
 import yaml
+import csv
 
 class SkyOptimization():
-    def __init__(self,yaml_file):
-        print("Initialize sky optimization")
+    def __init__(self,yaml_file,debug):
+        self.debug_ = debug
+        if self.debug_:
+            print("Initialize sky optimization")
+        self.yaml_file_ = yaml_file
+        self.relevant_cpu_list_ = []
+        self.cpu_benchmark_yaml_list_ = ['benchmark_cpu_min.yaml','benchmark_cpu_mid.yaml','benchmark_cpu_max.yaml']
+        self.yaml_file_contents_ = None
+        self.cpu_start_index_ = -1
+        self.cpu_candiate_index_ = -1
+        self.cpu_str_offset_ = 5
+        self.findCpuValues()
+        self.createBenchmarks()
+        #min_yaml_contents = yaml_file_contents
+        # f = open("demofile3.txt", "w")
+        # file_contents = ""
+        # for line in yaml_file_contents:
+        #     file_contents += line
+        # f.write(file_contents)
+        # f.close()
+
+        #parsed through_yaml = False
+        #print(yaml_file_contents[cpu_start_index])
+        #os.system("cat " + yaml_file_file)
+
+    def findCpuValues(self):
+        if self.debug_:
+            print("Finding relevant CPU values for fitting")
         cpu_start_string = '    candidates:\n'
         cwd = os.getcwd()
-        yaml_file = cwd + '/' + yaml_file
-        yaml_file_contents = None
-        with open(yaml_file, 'r') as file:
-            yaml_file_contents = file.readlines()
-        cpu_index = yaml_file_contents.index(cpu_start_string) + 1
+        self.yaml_file_ = cwd + '/' + self.yaml_file_
+        self.yaml_file_contents_ = None
+        with open(self.yaml_file_, 'r') as file:
+            self.yaml_file_contents_ = file.readlines()
+        cpu_index = self.yaml_file_contents_.index(cpu_start_string) + 1
+        self.cpu_start_index_ = cpu_index
         cpu_list = []
         yaml_parsed = False
+        self.cpu_str_offset_ = 5
         while(not yaml_parsed):
             try:
-                cpu_string = yaml_file_contents[cpu_index]
-                print(cpu_string)
-                cpu_substring = cpu_string[cpu_string.find("cpus:")+5:cpu_string.find("}")]
+                cpu_string = self.yaml_file_contents_[cpu_index]
+                cpu_substring = cpu_string[cpu_string.find("cpus:")+self.cpu_str_offset_:cpu_string.find("}")]
                 cpu_num = int(cpu_substring)
                 cpu_list.append(cpu_num)
                 cpu_index = cpu_index + 1
             except:
-                print("ITS OVER")
                 yaml_parsed = True
         cpu_list.sort()
         cpu_min = cpu_list[0]
         cpu_max = cpu_list[len(cpu_list) - 1]
         cpu_mid = cpu_list[int(len(cpu_list) / 2)]
-        print(cpu_min)
-        print(cpu_mid)
-        print(cpu_max)
-        #parsed through_yaml = False
-        #print(yaml_file_contents[cpu_start_index])
-        #os.system("cat " + yaml_file_file)
+        self.relevant_cpu_list_.append(cpu_min)
+        self.relevant_cpu_list_.append(cpu_mid)
+        self.relevant_cpu_list_.append(cpu_max)
+        if self.debug_:
+            print("CPU Min: " + str(cpu_min))
+            print("CPU Mid: " + str(cpu_mid))
+            print("CPU Max: " + str(cpu_max))
+
+    def createBenchmarks(self):
+        if self.debug_:
+            print("Creating benchmarks")
+        for i in range(len(self.cpu_benchmark_yaml_list_)):
+            cpu_string = self.yaml_file_contents_[self.cpu_start_index_]
+            cpu_substring = cpu_string[cpu_string.find("cpus:")+self.cpu_str_offset_:cpu_string.find("}")]
+            new_cpu_string = cpu_string.replace(cpu_substring,str(self.relevant_cpu_list_[i]))
+            self.yaml_file_contents_[self.cpu_start_index_] = new_cpu_string
+            f = open(self.cpu_benchmark_yaml_list_[i], "w")
+            file_contents = ""
+            for line in self.yaml_file_contents_:
+                file_contents += line
+            f.write(file_contents)
+            f.close()
+        
 
 def main():
     print("Hello World!")
     parser = argparse.ArgumentParser()
     parser.add_argument('-y', '--yaml_file', required=True)
+    parser.add_argument('-d','--debug', action='store_true')
+    parser.add_argument
     args = parser.parse_args()
     sky_optimization = SkyOptimization(**vars(args))
     # Start with reading the benchmark.yaml_file file
